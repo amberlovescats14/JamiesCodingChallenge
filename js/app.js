365e43330385b7c329f6757e1e7aca2ff3d84124
yaelBrown <yaelrbrown@gmail.com>
2019-09-09 12:04:46 -0500

    Finished assessment.

--- js/app.js
+++ js/app.js
@@ -14,6 +14,9 @@ var fetchUserData = () => {
 
 fetchUserData();
 
+
+
+
 // Generate avatars from random user data
 let avatar = [];
 
@@ -22,3 +25,65 @@ const generateAvatars = () => {
     avatar.push(e.picture.large);
   });
 }
+
+setTimeout(generateAvatars, 1000);
+
+
+
+
+// Get scores data from file
+var scores;
+fetch("../scores.json")
+  .then(response => response.json())
+  .then(json => {
+    scores = json;
+});
+
+
+
+
+// Add avatars to scores array
+
+const addAvatarsToScores = () => {
+  scores.forEach((e,i) => {
+    e.avatar = avatar[i];
+  });
+};
+
+setTimeout(addAvatarsToScores, 1100);
+
+
+
+
+// Display scores to page
+let displayScores = document.getElementById("displayScores");
+let html = "";
+
+const generateHTMLofScores = () => { 
+  for (let i = 0; i < 10; i++) {
+    html += `<div class='row'>`;
+    html += `<div class='username'>${scores[i].name}</div>`;
+    html += `<div class='score'>${scores[i].RSS}</div>`;
+    html += `<div class='avatar'><img src='${scores[i].avatar}'></div>`;
+    html += `</div>`;
+    html += "</br>";
+  }
+
+  displayScores.innerHTML = html;
+};
+
+setTimeout(generateHTMLofScores, 1200);
+
+
+
+
+
+
+
+
+
+
+
+
+displayScores.innerHTML = "hello";
+
